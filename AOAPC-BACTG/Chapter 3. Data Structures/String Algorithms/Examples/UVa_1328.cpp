6135a7a6ed5106f7b06c1bfdd378746e3ecfc11e
WinDaLex <fidd615@gmail.com>
2013-09-17 21:57:01 +0800

    modify UVa_1328

--- AOAPC-BACTG/Chapter 3. Data Structures/String Algorithms/Examples/UVa_1328.cpp
+++ AOAPC-BACTG/Chapter 3. Data Structures/String Algorithms/Examples/UVa_1328.cpp
@@ -3,8 +3,21 @@
 // by A Code Rabbit
 
 #include <cstdio>
+#include <cstring>
 
 const int MAXN = 1000002;
+
+namespace kmp {
+    void get_fail(char* p, int* f) {
+        int m = strlen(p);
+        f[0] = f[1] = 0;
+        for (int i = 1; i < m; i++) {
+            int j = f[i];
+            while (j && p[i] != p[j]) j = f[j];
+            f[i + 1] = p[i] == p[j] ? j + 1 : 0;
+        }
+    }
+}
 
 int T, N;
 char S[MAXN];
@@ -14,13 +27,8 @@ int f[MAXN];
 int main() {
     while (scanf("%d", &N) == 1 && N) {
         scanf("%s", S);
-        
-        f[0] = f[1] = 0;
-        for (int i = 1; i <= N; i++) {
-            int j = f[i];
-            while (j && S[j] != S[i]) j = f[j];
-            f[i + 1] = S[i] == S[j] ? j + 1 : 0;
-        }
+
+        kmp::get_fail(S, f);
 
         printf("Test case #%d\n", ++T);
         for (int i = 1; i <= N; i++)
