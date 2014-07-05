e3b07d0920ec1820a9911d6512ba973cdabe3de2
WinDaLex <fidd615@gmail.com>
2014-07-05 11:32:28 +0800

    Add CF_4A

---
+++ Codeforces/CF_4A.cpp
@@ -0,0 +1,14 @@
+// CF 4A
+// Watermelon
+// by A Code Rabbit
+
+#include <cstdio>
+
+int W;
+
+int main() {
+    scanf("%d", &W);
+    printf("%s\n", W > 2 && W % 2 == 0 ? "YES" : "NO");
+
+	return 0;
+}
